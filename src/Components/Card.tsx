6eb60b7432870d1e2cf69e0029b798e35a16156f
Sazid Ahsan <sazidahsan423@gmail.com>
2023-05-12 23:04:26 +0600

    ThreeDot menu created.

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,7 +1,7 @@
 import reminderImage from "../assets/Images/Icons_and_logos/reminder.svg";
 import cardBackgroundImage from "../assets/Images/Icons_and_logos/cardBackground.svg";
-import labelImage from "../assets/Images/Icons_and_logos/label.svg";
 import archiveImage from "../assets/Images/Icons_and_logos/archive.svg";
+import editImage from "../assets/Images/Icons_and_logos/edit.svg";
 import attachmentImage from "../assets/Images/Icons_and_logos/attachment.svg";
 import threeDotImage from "../assets/Images/Icons_and_logos/threeDot.svg";
 
@@ -24,16 +24,29 @@ export default function Card() {
             <img src={cardBackgroundImage} alt="cardBackground-image" />
           </span>
           <span className="elementInLowerPartOfCard">
-            <img src={labelImage} alt="label-image" />
+            <img src={archiveImage} alt="archive-image" />
           </span>
           <span className="elementInLowerPartOfCard">
-            <img src={archiveImage} alt="archive-image" />
+            <img src={editImage} alt="edit-image" />
           </span>
           <span className="elementInLowerPartOfCard">
             <img src={attachmentImage} alt="attachment-image" />
           </span>
           <span className="elementInLowerPartOfCard">
-            <img src={threeDotImage} alt="threeDot-image" />
+            <span className="threeDotMenuOfCard">
+              <img src={threeDotImage} alt="threeDot-image" />
+              <div className="threeDotItemsWraperInLowerPartOfCard">
+                <ul className="threeDotItemsInLowerPartOfCard">
+                  <li className="itemInThreeDotInLowerPartOfCard">Add label</li>
+                  <li className="itemInThreeDotInLowerPartOfCard">
+                    Make a copy
+                  </li>
+                  <li className="itemInThreeDotInLowerPartOfCard">
+                    Delete note
+                  </li>
+                </ul>
+              </div>
+            </span>
           </span>
         </div>
       </div>
