9abc09b434b806d809eed91f29b1fc4ef494500f
Sazid Ahsan <sazidahsan423@gmail.com>
2023-11-23 12:43:08 +0600

    Minor change made.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export default function middleware(request: NextRequest) {
-  // // Cookies
+  // Cookies
   const cookie: any = request.cookies.toString();
 
   // Conditions
