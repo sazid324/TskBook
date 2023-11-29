76495f45290552a3ebf09f38ac4ff8f10d89c539
Sazid Ahsan <sazidahsan423@gmail.com>
2023-11-29 19:52:11 +0600

    SignOut system created.

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,6 +3,7 @@
 // Library imports
 import Image from "next/image";
 import secureLocalStorage from "react-secure-storage";
+import Cookies from "universal-cookie";
 
 // CSS imports
 import style from "@/components/Profile/Profile.module.scss";
@@ -12,11 +13,30 @@ import profileImage from "../../../public/assets/Images/Icons/profile.svg";
 
 export default function Profile() {
   // Variables
+  const cookies = new Cookies();
+
   const UserData: any = secureLocalStorage.getItem("UserData");
   const ParsedUserData = JSON.parse(UserData);
   const FirstName: string = ParsedUserData ? ParsedUserData.first_name : "";
   const LastName: string = ParsedUserData ? ParsedUserData.last_name : "";
   const UserName: string = ParsedUserData ? ParsedUserData.username : "";
+
+  // Functions
+  const clickedOnSingOutButton = () => {
+    // Removed items from local storage
+    secureLocalStorage.removeItem("UserData");
+    secureLocalStorage.removeItem("nav-menu-state-in-local-storage");
+
+    // Removed items from session storage
+    sessionStorage.removeItem("usersJWTToken");
+
+    // Removed items from cookies
+    cookies.remove("usersRefreshToken");
+    cookies.remove("usersAccessToken");
+
+    // Redirecting page
+    window.location.href = "/signin";
+  };
 
   /////////////////////// Return Method ///////////////////////
 
@@ -52,7 +72,14 @@ export default function Profile() {
             <li className={`${style.userNameContainer} userNameContainer`}>
               <p className={`${style.userName} userName`}>{UserName}</p>
             </li>
-            <li className={`${style.signOutButton} signOutButton`}>Sign out</li>
+            <li
+              className={`${style.signOutButton} signOutButton`}
+              onClick={() => {
+                clickedOnSingOutButton();
+              }}
+            >
+              Sign out
+            </li>
           </ul>
         </div>
       </button>
